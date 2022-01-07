899432d4dc854f691c919ab15463e6689631fdbe
Stefan Schulz <97118554+KERNREAKTOR@users.noreply.github.com>
2022-01-07 14:06:02 +0100

    Add files via upload

---
+++ src/MapInfo.java
@@ -0,0 +1,88 @@
+
+
+public class MapInfo {
+	public String GetMapName (String apimapname)
+	{
+		String[] apimapnames = 
+			{
+				"CausticValley",
+				"Crater2",
+				"CrimsonStrait",
+				"EmeraldTaiga",
+				"ForestColony",
+				"ForestColonyClassic",
+				"ForestColonySnow",
+				"FrozenCity",
+				"FrozenCityClassic",
+				"FrozenCityNight",
+				"GrimPlexus",
+				"GrimPortico",
+				"HelleboreSprings",
+				"HelleboreSpringsQP",
+				"HibernalRift",
+				"HPGManifold",
+				"IceLavaNetwork",
+				"IshiyamaCaves",
+				"LiaoJungle",
+				"MechFactory",
+				"MiningCollective",
+				"PolarHighlands",
+				"PrivateTest1v1",
+				"PrivateTest2v2",
+				"PrivateTest4v4",
+				"PrivateTest8ffa",
+				"RiverCity",
+				"RubelliteOasis",
+				"SteinerColiseum",
+				"SulfurousRift",
+				"TerraTherma",
+				"TourmalineDesert",
+				"ViridianBog",
+				"VitricForge"
+			};
+		
+		String[] ingamemapnames =
+			{
+				"Caustic Valley",
+				"Rubellite Oasis",
+				"Crimson Strait",
+				"Emerald Taiga",
+				"Forest Colony",
+				"Forest Colony (Classic)",
+				"Forest Colony Snow (Classic)",
+				"Frozen City",
+				"Frozen City (Classic)",
+				"Frozen City Night (Classic)",
+				"Grim Plexus",
+				"Grim Portico",
+				"Hellebore Springs",
+				"Hellebore Outpost",
+				"HIBERNAL RIFT",
+				"HPG Manifold",
+				"Hibernal Rift",
+				"Ishiyama Caves",
+				"Liao Jungle",
+				"Mech Factory",
+				"Mining Collective",
+				"Polar Highlands",
+				"1v1 Steiner Coliseum",
+				"2v2 Test",
+				"4v4 Test A",
+				"4v4 Test B",
+				"River City",
+				"Rubellite Oasis",
+				"Steiner Coliseum",
+				"Sulfurous Rift",
+				"Terra Therma",
+				"Tourmaline Desert",
+				"Viridian Bog",
+				"Vitric Forge"
+			};
+		
+
+	return "TEST";
+		
+	}
+	
+
+}
