5dfbd67d651d4895a032afbbbafb9a5b8d877eb5
Kernreaktor <mail-an-kernreaktor@web.de>
2022-01-07 10:23:08 +0100

    Test

---
+++ src/TestingClass.java
@@ -0,0 +1,24 @@
+public class TestingClass {
+
+    public static void main(String[] args)
+    {
+        Integer mechItemID =513;
+
+        MechIdInfo mechIdInfo = new MechIdInfo();
+        //
+        Integer tonnage = mechIdInfo.getTonnage(mechItemID); // 90
+        String faction = mechIdInfo.getFaction(mechItemID).toString(); //CLAN
+        String chassis = mechIdInfo.getChassis(mechItemID);//SUPERNOVA
+        String mechclass = mechIdInfo.getMechclass(mechItemID).toString();//ASSAULT
+        String varianttype = mechIdInfo.getVarianttype(mechItemID);//HERO
+        String fullname = mechIdInfo.getFullname(mechItemID);//BOILER
+        String shortname = mechIdInfo.getShortname(mechItemID);// SNV-BR
+
+
+       MapInfo mapname = new MapInfo();
+
+       System.out.println(mapname.GetMapName("CapitolCity"));
+       System.out.println(mechIdInfo.getFullname(45));
+
+    }
+}
